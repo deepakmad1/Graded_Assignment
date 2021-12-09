ef36b075d5de9314acb139f480f57c74e3855383
deepak h <deepak_h@hcl.com>
2021-12-09 23:57:58 +0530

    graded_assignment

---
+++ src/main/java/com/boot/demo/GradedAssignment10Application.java
@@ -0,0 +1,14 @@
+package com.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradedAssignment10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradedAssignment10Application.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
